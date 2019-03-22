51beab95b082465139c09f2051c06e3bb3322afe
Hüsler Etienne <etienne.husler@he-arc.ch>
2019-03-22 09:53:47 +0100

    Corrected Test Suites path

--- runTest.sh
+++ runTest.sh
@@ -4,4 +4,4 @@ Xvfb :99 &
 export local_addr="127.0.0.1:8080"
 
 
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/PinaPollKatalon/PinaPollKatalon.prj" -retry=1 -testSuitePath="Test Suites/SpringTest" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/PinaPollKatalon/PinaPollKatalon.prj" -retry=1 -testSuitePath="Test Suites/Test Suite" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
