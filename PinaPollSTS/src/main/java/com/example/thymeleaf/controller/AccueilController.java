60558275453c99a0b40a193a1752bc1df123cf58
Hüsler Etienne <etienne.husler@he-arc.ch>
2019-03-22 12:07:29 +0100

    Base Project with welcome page

--- PinaPollSTS/src/main/java/com/example/thymeleaf/controller/AccueilController.java
+++ PinaPollSTS/src/main/java/com/example/thymeleaf/controller/AccueilController.java
@@ -2,26 +2,13 @@ package com.example.thymeleaf.controller;
 
 import java.util.Map;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.example.thymeleaf.data.ColorsDAO;
-import com.example.thymeleaf.model.Color;
 
 @Controller
 public class AccueilController {
-
-		@Autowired
-		ColorsDAO dao;
-		
+	
 		@Value("${accueil.message:test}")
 		private String message;
 		
@@ -32,36 +19,6 @@ public class AccueilController {
 		public String accueil(Map<String, Object> model) {
 			model.put("page", "Accueil");
 			return "accueil";
-		}
-		
-		@GetMapping("/basic")
-		public String basic(Map<String, Object> model) {
-			model.put("message", this.message);
-			model.put("page", "Concepts de bases");
-			
-			model.put("message", this.message);
-			model.put("messageParDefaut", this.messageParDefaut);
-			model.put("page", "Concepts de bases");
-			
-			
-			model.put("couleurs", dao.getAllColors());
-			return "basic";
-		}
-		
-		@GetMapping("/form")
-		public String form(Map<String, Object> model) {
-			model.put("color", new Color());
-			model.put("page", "Formulaire");
-			return "formulaire";
-		}
-		
-		@PostMapping("/color")
-		public String saveColor(@Valid @ModelAttribute Color color, BindingResult errors, Model model) {
-			
-			if (!errors.hasErrors()) {
-	            dao.save(color);
-	        }
-	        return ((errors.hasErrors()) ? "color" : "redirect:basic");
 		}
 }
 
