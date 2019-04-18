d0fbfbaa6566d3f26cfb0043c7d80ba5fdc59703
fabien.vorpe <fabien.vorpe@he-arc.ch>
2019-04-18 17:52:09 +0200

    delete some useless classes

--- PinaPollSTS/src/main/java/com/pinapoll/repositories/UserResponseRepository.java
+++
@@ -1,5 +0,0 @@
-package com.pinapoll.repositories;
-
-public class UserResponseRepository {
-
-}
--- PinaPollSTS/src/main/java/com/pinapoll/services/UserResponseService.java
+++
@@ -1,7 +0,0 @@
-package com.pinapoll.services;
-
-import com.pinapoll.models.UserResponse;
-
-public class UserResponseService {
-
-}
