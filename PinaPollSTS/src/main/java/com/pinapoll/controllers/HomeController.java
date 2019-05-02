d87523c1103b33cfb7107a4a2c5c5b218acbce68
Etienne Hüsler <etienne.husler@he-arc.ch>
2019-05-02 16:21:22 +0200

    Removed some code smells (unused imports)

--- PinaPollSTS/src/main/java/com/pinapoll/controllers/HomeController.java
+++ PinaPollSTS/src/main/java/com/pinapoll/controllers/HomeController.java
@@ -1,23 +1,17 @@
 package com.pinapoll.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.naming.directory.SearchControls;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pinapoll.models.Poll;
 import com.pinapoll.models.User;
-import com.pinapoll.services.PollService;
 import com.pinapoll.services.PollServiceImpl;
 import com.pinapoll.services.UserService;
 
--- PinaPollSTS/src/main/java/com/pinapoll/services/CategoryServiceImpl.java
+++ PinaPollSTS/src/main/java/com/pinapoll/services/CategoryServiceImpl.java
@@ -1,26 +1,19 @@
 package com.pinapoll.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pinapoll.models.Category;
-import com.pinapoll.repositories.CategoryRepository;
 
 @Service("categoryService")
 public class CategoryServiceImpl implements CategoryService{
 
-	@Autowired
-	private CategoryRepository categoryRepository;
-
 	@Override
 	public Category getCategory(int id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void saveCategory(Category category) {
-		// TODO Auto-generated method stub
 		
 	}
 }
