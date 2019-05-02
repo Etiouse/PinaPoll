f655e10c4b4a5cf3b65202b7d28ea871b9dc7484
Etienne Hüsler <etienne.husler@he-arc.ch>
2019-05-02 22:43:37 +0200

    Removed unused imports from last merge

--- PinaPollSTS/src/main/java/com/pinapoll/controllers/PollController.java
+++ PinaPollSTS/src/main/java/com/pinapoll/controllers/PollController.java
@@ -1,13 +1,7 @@
 package com.pinapoll.controllers;
 
-import java.awt.print.Printable;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
